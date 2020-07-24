723627de92a4b99ed983f34c6b7245c086573686
Phong D. Nguyen <phongnguyenprograms@gmail.com>
2020-07-24 15:36:42 -0700

    added store model s per function g routes yles

---
+++ models/Store.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let keystone = require('keystone');
+let Types = keystone.Field.Types;
+
+/**
+ * Store Model
+ * ==========
+ */
+
+let Store = new keystone.List('Store', {
+    autokey: { path: 'slug', from: 'name', unique: true },
+    track: true
+});
+
+Store.add({
+    name: { type: Types.Text },
+    address: { type: Types.Location, defaults: { country: 'USA' } },
+    phone: { type: Types.Number },
+    open: { type: Types.Html, wysiwyg: true},
+});
+
+Store.register();
